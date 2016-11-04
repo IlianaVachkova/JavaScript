1cd1d9599c043aeefd82635ec67f72d918c45f13
IlianaVachkova <an_vachkova@yahoo.com>
2016-11-04 18:46:23 +0200

    Added homework FunctionsAndFunctionExpressions.

---
+++ JavaScript OOP/FunctionsAndFunctionExpressions/FunctionsAndFunctionExpressions/func-task1.js
@@ -0,0 +1,19 @@
+﻿function solve() {
+    return function sum(numbers) {
+        if (numbers.length === 0) {
+            return null;
+        }
+
+        return numbers.reduce(function (s, n) {
+            n = +n;
+
+            if (isNaN(n)) {
+                throw new Error();
+            }
+
+            return s + n;
+        }, 0);
+    }
+
+    module.exports = sum;
+}
