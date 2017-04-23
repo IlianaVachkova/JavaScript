c5d1685213db1aa213565906cefdfe25b59577be
Iliana Vachkova <an_vachkova@yahoo.com>
2017-04-23 23:15:49 +0300

    Added SPA with JavaScript homeworks

---
+++ SPA with JavaScript/Handlebars/SimpleForum/public/scripts/routing.js
@@ -0,0 +1,35 @@
+﻿import { data } from './data.js'
+import { templateLoader as tl} from './template-loader.js'
+
+var router = (() => {
+    let navigo;
+
+    //for default routing
+    function init(){
+        navigo=new Navigo(null, false);
+
+        navigo
+            .on('/threads/:id', (params)=> {
+                Promise.all([data.threads.getById(), tl.get('messages')])
+                      .then(([data, template])=>$('#content').html(template(data)))
+                      .catch(console.log)
+            })
+            .on('/threads', ()=>{
+                Promise.all([data.threads.get(), tl.get('threads')])
+                      .then(([data, template])=>$('#content').html(template(data)))
+                      .catch(console.log)
+        })
+            .on('/gallery', ()=>{
+                Promise.all([data.gallery.get(), tl.get('gallery')])
+                  .then(([data, template])=>$('#content').html(template(data)))
+                  .catch(console.log)
+            }).resolve();
+    }
+
+
+    return {
+        init
+    }
+})();
+
+export { router };
---
+++ SPA with JavaScript/PromisesAndAsynchronousProgramming/PopupThenLoad/popup-load.js
@@ -0,0 +1,34 @@
+﻿window.onload = function () {
+    'use strict';
+
+    function fadeIn() {
+        var timer,
+            popupDiv = document.getElementById('popup'),
+            initialOpacity = 0,
+            increment = 0.005,
+            intervalMs = 30;
+
+        popupDiv.style.opacity = initialOpacity;
+        popupDiv.textContent = 'Redirecting to Stack Overflow';
+
+        return new Promise(function (resolve) {
+            timer = setInterval(function () {
+                if (popupDiv.style.opacity >= 1) {
+                    clearInterval(timer);
+                    resolve();
+                }
+
+                popupDiv.style.opacity = initialOpacity;
+                initialOpacity += increment;
+            }, intervalMs);
+        });
+    }
+
+    function redirect() {
+        window.location = 'http://stackoverflow.com/questions/32811884/javascript-promises';
+    }
+
+    fadeIn()
+        .then(redirect);
+
+};
